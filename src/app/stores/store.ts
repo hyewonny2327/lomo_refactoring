12842dfd7ed6cc05fd29dcc68edc2e48b098b98b
hyewon <wonny2327@gamil.com>
2024-11-29 17:17:38 +0900

    refactor:refactor setFinalAvatarId

--- src/app/stores/store.ts
+++ src/app/stores/store.ts
@@ -23,47 +23,36 @@ const useAvatarStore = create<AvatarStore>((set) => ({
     set(() => ({
       avatarIds: [...ids],
     })),
-  setFinalAvatarId: (index: number, value: number) =>
+  setFinalAvatarId: (index, value) =>
     set((state) => {
+      const { step, finalAvatarId, avatarIds } = state;
       // 1. finalAvatarId 배열 업데이트
-      const updatedFinalAvatarId = [...state.finalAvatarId];
-      if (state.step === 2) {
-        updatedFinalAvatarId[index - 1] = value;
-      } else {
-        updatedFinalAvatarId[index - 1] = value + 1;
-      }
-      let newAvatarIds = [...state.avatarIds];
-      //마지막 단계에서는 avatarIds를 업데이트하지 않음
-      if (state.step < 5) {
-        // 2. avatarIds 배열 업데이트
-        if (state.step === 1) {
-          // Step이 1일 때: 1번 인덱스 변경
+      const updatedFinalAvatarId = [...finalAvatarId];
+      updatedFinalAvatarId[index - 1] = value + (step !== 2 ? 1 : 0);
+      // 2. avatarIds 배열 업데이트 (step < 5일 때만)
+      if (step < 5) {
+        let newAvatarIds = [...avatarIds];
+        if (step === 1) {
+          // Step이 1일 때: 첫 번째 인덱스 업데이트
           const prefix = updatedFinalAvatarId[0];
           const suffix = updatedFinalAvatarId.slice(2).join('');
-          newAvatarIds = [
-            `${prefix}0${suffix}`,
-            `${prefix}1${suffix}`,
-            `${prefix}0${suffix}`,
-            `${prefix}0${suffix}`,
-            `${prefix}0${suffix}`,
-          ];
-        } else if (state.step >= 2) {
-          // Step이 2 이상일 때: 현재 step 인덱스를 업데이트
-          newAvatarIds = state.avatarIds.map((id, idx) => {
-            // 현재 step에 해당하는 인덱스만 변경
-
-            const updatedId = updatedFinalAvatarId.slice(0, state.step).join('') + (idx + 1) + id.slice(state.step + 1);
-
-            return updatedId;
-          });
+          newAvatarIds = Array(5)
+            .fill('')
+            .map((_, idx) => `${prefix}${idx}${suffix}`);
+        } else if (step >= 2) {
+          // Step이 2 이상일 때: step에 해당하는 부분 업데이트
+          newAvatarIds = avatarIds.map(
+            (id, idx) => updatedFinalAvatarId.slice(0, step).join('') + (idx + 1) + id.slice(step + 1)
+          );
         }
         return {
           finalAvatarId: updatedFinalAvatarId,
           avatarIds: newAvatarIds,
         };
-      } else {
-        return { finalAvatarId: updatedFinalAvatarId };
       }
+
+      // Step이 5 이상일 경우 avatarIds는 변경하지 않음
+      return { finalAvatarId: updatedFinalAvatarId };
     }),
 }));
 
