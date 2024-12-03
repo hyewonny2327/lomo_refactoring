6fda08dded6813f19ca33f456061dc64e6f05e71
hyewon <wonny2327@gamil.com>
2024-12-03 15:10:32 +0900

    seed avatar data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,45 @@
 // import { fakerKO as faker } from '@faker-js/faker';
-import { prisma } from '../src/app/lib/prisma';
+// import prisma from '@/app/db';
+import { PrismaClient } from '@prisma/client';
+import fs from 'fs/promises';
+interface AvatarNumber {
+  name: string;
+  numbers: number[];
+}
 
+interface BodyType {
+  id: number; // Optional if not used in seeding
+  type: string;
+  avatarNumbers: AvatarNumber[];
+}
+interface TextBlock {
+  type: string;
+  summary: string;
+  stylingTips: stylingTip;
+}
+interface stylingTip {
+  description: string;
+  tips: tip[];
+}
+interface tip {
+  category: string;
+  description: string;
+}
 // type UserType = {
 //   id: string;
 //   email?: string | null;
 //   password?: string | null;
 // };
+const prisma = new PrismaClient();
+async function readJSONFile(filePath: string) {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    return JSON.parse(data); // JSON 데이터 반환
+  } catch (error) {
+    console.error(`Failed to read file ${filePath}:`, error);
+    throw error;
+  }
+}
 
 async function seedUsers() {
   const userData = Array.from({ length: 1 }).map(() => ({
@@ -22,9 +56,58 @@ async function seedUsers() {
     console.error('Error seeding users:', error);
   }
 }
+async function seedBodyType() {
+  const bodyTypeData: BodyType[] = await readJSONFile('prisma/bodyType.json');
+  await Promise.all(
+    bodyTypeData.map((bodyType) =>
+      prisma.bodyType.create({
+        data: {
+          type: bodyType.type,
+          avatarNumbers: {
+            create: bodyType.avatarNumbers.map((avatar) => ({
+              name: avatar.name,
+              numbers: avatar.numbers,
+            })),
+          },
+        },
+      })
+    )
+  );
+
+  console.log('BodyType array seeded!');
+}
+
+async function seedTextBlock() {
+  const textBlockData: TextBlock[] = await readJSONFile('prisma/textBlock.json');
+  await Promise.all(
+    textBlockData.map((textBlock: TextBlock) =>
+      prisma.bodyType.create({
+        data: {
+          type: textBlock.type,
+          summary: textBlock.summary,
+          stylingTips: {
+            create: {
+              description: textBlock.stylingTips.description,
+              tips: {
+                create: textBlock.stylingTips.tips.map((tip: tip) => ({
+                  category: tip.category,
+                  description: tip.description,
+                })),
+              },
+            },
+          },
+        },
+      })
+    )
+  );
+
+  console.log('TextBlock array seeded!');
+}
 
 async function main() {
-  await seedUsers();
+  // await seedUsers();
+  await seedBodyType();
+  await seedTextBlock();
 
   // seedRooms()
 }
