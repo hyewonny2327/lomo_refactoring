a74db5f6d1f60abcabf9e60c35b92cec2a0bf0b7
hyewon <wonny2327@gamil.com>
2024-12-14 17:59:41 +0900

    feat: loading 컴포넌트-페이지

--- src/app/(pages)/select/result/page.tsx
+++ src/app/(pages)/select/result/page.tsx
@@ -10,6 +10,7 @@ import Button from '@/app/components/Button';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { updateAvatarNumber } from '@/app/services/client/updateAvatarNumber';
+import Loading from '@/app/components/Loading';
 const ResultPage = () => {
   const { finalAvatarId, resetStore } = useAvatarStore();
   const resultNumber = Number(finalAvatarId.join(''));
@@ -71,83 +72,73 @@ const ResultPage = () => {
   }
 
   const isLoading = avatarImage.url === '';
+  // const isLoading = true;
 
   return (
-    <div className={styles.resultPage}>
-      <section className={styles.resultPage__avatarImageContainer}>
-        <Image
-          className={styles.resultPage__avatarImageContainer__logo}
-          src="/logo.svg"
-          alt="LOMO logo"
-          width={141}
-          height={41}
-          priority
-        />
-        <header className={styles.resultPage__avatarImageContainer__title}>
-          {isLoading ? (
-            <div className={styles.resultPage__avatarImageContainer__title__upper}>잠시만 기다려주세요</div>
-          ) : (
-            <>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className={styles.resultPage}>
+          <section className={styles.resultPage__avatarImageContainer}>
+            <Image
+              className={styles.resultPage__avatarImageContainer__logo}
+              src="/logo.svg"
+              alt="LOMO logo"
+              width={141}
+              height={41}
+              priority
+            />
+            <header className={styles.resultPage__avatarImageContainer__title}>
               <div className={styles.resultPage__avatarImageContainer__title__upper}>{avatarInfo.upperType}</div>
               <div className={styles.resultPage__avatarImageContainer__title__lower}>{avatarInfo.lowerType}</div>
-            </>
-          )}
-        </header>
-        <main>
-          {isLoading ? (
-            <Image
-              className={styles.resultPage__avatarImageContainer__image}
-              src="/loadingAvatar.svg"
-              alt="Avatar"
-              layout="intrinsic"
-              width={174}
-              height={98}
-            />
-          ) : (
-            <Image
-              className={styles.resultPage__avatarImageContainer__image}
-              src={avatarImage.url}
-              alt="Avatar"
-              layout="intrinsic"
-              width={174}
-              height={98}
-            />
-          )}
-        </main>
-      </section>
-      <section className={styles.resultPage__textContainer}>
-        <div className={styles.resultPage__textContainer__title}>1. 상체 분석</div>
-        <div>{upperTypeData?.summary}</div>
-        <div className={styles.resultPage__textContainer__subtitle}>스타일링 TIP!</div>
-        <div className={styles.resultPage__textContainer__stylingTip}>{upperTypeData?.stylingTips.description}</div>
-        {upperTypeData?.stylingTips.tips.map((item, id) => (
-          <div key={id} className={styles.resultPage__textContainer__tipsBlock}>
-            <span className={styles.resultPage__textContainer__tipsBlock__category}>{item.category} :</span>
-            <span className={styles.resultPage__textContainer__tipsBlock__description}>{item.description}</span>
-          </div>
-        ))}
-      </section>
-      <section className={styles.resultPage__textContainer}>
-        <div className={styles.resultPage__textContainer__title}>2. 하체 분석</div>
-        <div>{lowerTypeData?.summary}</div>
-        <div className={styles.resultPage__textContainer__subtitle}>스타일링 TIP!</div>
-        <div className={styles.resultPage__textContainer__stylingTip}>{lowerTypeData?.stylingTips.description}</div>
-        {lowerTypeData?.stylingTips.tips.map((item, id) => (
-          <div key={id} className={styles.resultPage__textContainer__tipsBlock}>
-            <span className={styles.resultPage__textContainer__tipsBlock__category}>{item.category} :</span>
-            <span className={styles.resultPage__textContainer__tipsBlock__description}>{item.description}</span>
-          </div>
-        ))}
-      </section>
-      <section className={styles.resultPage__buttonContainer}>
-        <Button type="colored" size="large" color="primary">
-          결과 공유하기
-        </Button>
-        <Button type="outlined" size="large" color="black" onClick={handleClickHomeButton}>
-          처음으로 돌아가기
-        </Button>
-      </section>
-    </div>
+            </header>
+            <main>
+              <Image
+                className={styles.resultPage__avatarImageContainer__image}
+                src={avatarImage.url}
+                alt="Avatar"
+                layout="intrinsic"
+                width={174}
+                height={98}
+              />
+            </main>
+          </section>
+          <section className={styles.resultPage__textContainer}>
+            <div className={styles.resultPage__textContainer__title}>1. 상체 분석</div>
+            <div>{upperTypeData?.summary}</div>
+            <div className={styles.resultPage__textContainer__subtitle}>스타일링 TIP!</div>
+            <div className={styles.resultPage__textContainer__stylingTip}>{upperTypeData?.stylingTips.description}</div>
+            {upperTypeData?.stylingTips.tips.map((item, id) => (
+              <div key={id} className={styles.resultPage__textContainer__tipsBlock}>
+                <span className={styles.resultPage__textContainer__tipsBlock__category}>{item.category} :</span>
+                <span className={styles.resultPage__textContainer__tipsBlock__description}>{item.description}</span>
+              </div>
+            ))}
+          </section>
+          <section className={styles.resultPage__textContainer}>
+            <div className={styles.resultPage__textContainer__title}>2. 하체 분석</div>
+            <div>{lowerTypeData?.summary}</div>
+            <div className={styles.resultPage__textContainer__subtitle}>스타일링 TIP!</div>
+            <div className={styles.resultPage__textContainer__stylingTip}>{lowerTypeData?.stylingTips.description}</div>
+            {lowerTypeData?.stylingTips.tips.map((item, id) => (
+              <div key={id} className={styles.resultPage__textContainer__tipsBlock}>
+                <span className={styles.resultPage__textContainer__tipsBlock__category}>{item.category} :</span>
+                <span className={styles.resultPage__textContainer__tipsBlock__description}>{item.description}</span>
+              </div>
+            ))}
+          </section>
+          <section className={styles.resultPage__buttonContainer}>
+            <Button type="colored" size="large" color="primary">
+              결과 공유하기
+            </Button>
+            <Button type="outlined" size="large" color="black" onClick={handleClickHomeButton}>
+              처음으로 돌아가기
+            </Button>
+          </section>
+        </div>
+      )}
+    </>
   );
 };
 
